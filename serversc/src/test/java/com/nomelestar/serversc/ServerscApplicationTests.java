46bd06b5b06d4c4f7718c46f9acab563f752f6ad
Sebastian Zapata <pansezapata@gmail.com>
2026-02-16 08:37:08 -0500

    Se crean los microservicios

---
+++ serversc/src/test/java/com/nomelestar/serversc/ServerscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomelestar.serversc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

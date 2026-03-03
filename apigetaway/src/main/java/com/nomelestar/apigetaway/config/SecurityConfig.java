62e75ee8d5639a5a40cd4e4e81576dbebc461f8e
Sebastian Zapata <pansezapata@gmail.com>
2026-03-03 15:07:39 -0500

    se crea para cuadrar login

--- apigetaway/src/main/java/com/nomelestar/apigetaway/config/SecurityConfig.java
+++ apigetaway/src/main/java/com/nomelestar/apigetaway/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.JwtIssuerValidator;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,8 +21,20 @@ public class SecurityConfig {
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/actuator/**", "/eureka/**").permitAll()
                         .anyExchange().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
-                }));
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtDecoder(reactiveJwtDecoder())));
         return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        // We use the internal docker network URL to fetch the certificates
+        String jwkSetUri = "http://keycloak:8080/realms/nomelestar/protocol/openid-connect/certs";
+        NimbusReactiveJwtDecoder jwtDecoder = NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
+
+        // But we validate the token's "iss" claim against the external URL Postman used
+        // to get it
+        jwtDecoder.setJwtValidator(new JwtIssuerValidator("http://localhost:9090/realms/nomelestar"));
+
+        return jwtDecoder;
     }
 }
